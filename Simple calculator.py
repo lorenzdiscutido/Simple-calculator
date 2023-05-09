98ff6fdb8ac101be93966b4dcfdacd8338f5f8d6
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-09 20:55:00 +0800

    Added a addition feature to the program

--- Simple calculator.py
+++ Simple calculator.py
@@ -5,6 +5,21 @@ print("Type 2 if you want to use Subtraction")
 print("Type 3 if you want to use Multiplication")
 print("Type 4 if you want to use Division")
 #Ask the user what operation they want to use
+while True:
+    operation = input("What operation would you like to use?: ")
+
+    #check if the choice of the user is between 1-4
+    if operation in ("1", "2", "3", "4"):
+
+        #Add the numbers
+        if operation == "1":
+            print("Choice of operation: Addition")
+            try:
+                #Ask for their first and second number
+                num1 = float(input("Please enter your first number:"))
+                num2 = float(input("Please enter your second number:"))
+            except ValueError:
+                print("Invalid. Please try again")
 #Make the user input numbers
 #Add the numbers
 #Subtract the numbers
