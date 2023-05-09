128ed52b8a0371429f33a75ee30162f9cbba7d20
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-09 21:34:14 +0800

    Code is working fine but does not stop looping

--- Simple calculator.py
+++ Simple calculator.py
@@ -66,12 +66,3 @@ while True:
         print("Thank you for using this calculator")  
     else:
         print("Proceeding to another calculation")    
-#Make the user input numbers
-#Add the numbers
-#Subtract the numbers
-#Multiply the numbers
-#Divide the number
-#Print the result of the operation
-#Ask the user if they want to calculate another set of numbers
-#End the program if the user wished to.
-#Error handling
