4a3df55a26e0d9052f12ff713450c84e358c7589
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-09 20:39:00 +0800

    Pseudocodes

---
+++ Simple calculator.py
@@ -0,0 +1,10 @@
+#Ask the user what operation they want to use
+#Make the user input numbers
+#Add the numbers
+#Subtract the numbers
+#Multiply the numbers
+#Divide the number
+#Print the result of the operation
+#Ask the user if they want to calculate another set of numbers
+#End the program if the user wished to.
+#Error handling
