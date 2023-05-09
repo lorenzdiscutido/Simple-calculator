8929ea6c8181f251301ab1e8bd98efbd34950070
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-05-09 20:45:31 +0800

    Guide for the user when choosing operation

--- Simple calculator.py
+++ Simple calculator.py
@@ -1,3 +1,9 @@
+#Guide
+print("Calculator operation")
+print("Type 1 if you want to use Addition")
+print("Type 2 if you want to use Subtraction")
+print("Type 3 if you want to use Multiplication")
+print("Type 4 if you want to use Division")
 #Ask the user what operation they want to use
 #Make the user input numbers
 #Add the numbers
